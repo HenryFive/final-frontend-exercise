503c5f0f6d3e94ef8dda1d3e75ff878624f2c75b
Henry James Cinco <henryjamescinco@gmail.com>
2021-05-24 14:34:27 +0800

    commit to resole pull

---
+++ js/login.js
@@ -0,0 +1,11 @@
+class user {
+    constructor( fullName, email, password, dateRegistered) {
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+        this.dateRegistered = dateRegistered;
+    }
+    timeStamp() {
+        
+    }
+  }
